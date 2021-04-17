80a3935f81753b49f1056172f28144d721976f8b
Christian Preß <cpress@it4ipm.de>
2021-04-17 21:24:39 +0200

    Fully Qualified Class Name, Static Type Import and Static Import on Demand - start

---
+++ getting-started/src/TestImportStatic.java
@@ -0,0 +1,22 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 2: Testing Import static statements.
+*/
+
+import static java.lang.Math.PI;
+
+public class TestImportStatic {
+    public static void main(String[] args) {
+        int radius = 2;
+
+        // We can use a static class variable on the Math class to
+        // get the value of PI
+        double circumference = 2 * PI * radius;
+        System.out.println("circumference = " + circumference);
+
+        // We can use a static class method on the Math class to
+        // get the square root of a number
+        double sqrt81 = java.lang.Math.sqrt(81);
+        System.out.println("sqrt81 = " + sqrt81);
+    }
+}
