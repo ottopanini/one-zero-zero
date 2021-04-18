d7bb07d2a09f76a51cfd50586dfd5a9f09ba5ebe
Christian Preß <cpress@it4ipm.de>
2021-04-18 13:50:56 +0200

    add variable definitions with underscore use

--- primitive-data/src/Foo2.java
+++ primitive-data/src/Foo2.java
@@ -70,5 +70,20 @@ public class Foo2 {
         char c6 = '\u007f';
         System.out.println("b6 = " + b6);
         System.out.println("c6 = " + c6);
+
+        byte b7 = 0b0000_0001;
+        char c7 = 0x0_07F;
+        short s7 = 011_11;
+        int i7 = 1_000_000;
+        long d7 = 1_000_000L;
+        float f7 = 1_000.000_000f;
+        double l7 = 1.000_0000e10;
+        System.out.println("b7 = " + b7);
+        System.out.println("c7 = " + c7);
+        System.out.println("s7 = " + s7);
+        System.out.println("i7 = " + i7);
+        System.out.println("l7 = " + l7);
+        System.out.println("f7 = " + f7);
+        System.out.println("d7 = " + d7);
     }
 }
