7c3f0f51c572802b9a315562717befabdca1b2aa
Christian Preß <cpress@it4ipm.de>
2021-04-18 19:59:53 +0200

    add local variable type interference - add some valids

--- lvti/src/VarTest.java
+++ lvti/src/VarTest.java
@@ -11,5 +11,19 @@ public class VarTest {
         var aClassWithAVeryLongName = new AClassWithAVeryLongName();
         aClassWithAVeryLongName.setName("Testing");
         System.out.println(aClassWithAVeryLongName);
+
+        // Adding some other var declarations:
+        // i is inferred to be an int, since it's assigned a literal int
+        var i = 1;
+
+        // An array can be assigned to an LVTI variable
+        var aVarArray = new int[3];
+
+        // Valid to assign a method return value to an LVTI variable
+        var methodVal = aClassWithAVeryLongName.getName();
+
+        // OK to assign a null object to LVTI variable but not literal null
+        Object nullObject = null;
+        var var = nullObject;
     }
 }
