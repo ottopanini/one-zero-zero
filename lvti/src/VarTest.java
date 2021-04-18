b01d3e3c9a9589bdbe0fda98640af9ce80b802e9
Christian Preß <cpress@it4ipm.de>
2021-04-18 19:54:39 +0200

    add local variable type interference

---
+++ lvti/src/VarTest.java
@@ -0,0 +1,14 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 3: Working with Java Primitive Data Types and String APIs
+Topic: Local Variable Type Inference
+*/
+
+public class VarTest {
+    public static void main(String[] args) {
+        com.WordyCompanyName.ALongPackageName.AClassWithAVeryLongName aClassWithAVeryLongName =
+                new com.WordyCompanyName.ALongPackageName.AClassWithAVeryLongName();
+        aClassWithAVeryLongName.setName("Testing");
+        System.out.println(aClassWithAVeryLongName);
+    }
+}
