ca01af9c7120f5b88cf8784a0c8fd30d9a974f4d
Christian Preß <cpress@it4ipm.de>
2021-04-18 13:42:49 +0200

    add variable definitions with type specifier suffix

--- primitive-data/src/Foo2.java
+++ primitive-data/src/Foo2.java
@@ -30,6 +30,19 @@ public class Foo2 {
         System.out.println("d = " + d);
         System.out.println("isTrue = " + isTrue);
 
+        long l2 = 127l;
+        long l3 = 127L;
+        System.out.println("l2 = " + l2);
+        System.out.println("l2 = " + l3);
 
+        float f2 = 127f;
+        float f3 = 127F;
+        System.out.println("f2 = " + f2);
+        System.out.println("f3 = " + f3);
+
+        double d2 = 127d;
+        double d3 = 127D;
+        System.out.println("d2 = " + d2);
+        System.out.println("d3 = " + d3);
     }
 }
