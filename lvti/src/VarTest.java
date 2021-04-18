cc16a85b3631a0aa8b3a77404a55cc602fde17b8
Christian Preß <cpress@it4ipm.de>
2021-04-18 20:03:53 +0200

    add local variable type interference - add some invalids

--- lvti/src/VarTest.java
+++ lvti/src/VarTest.java
@@ -25,5 +25,25 @@ public class VarTest {
         // OK to assign a null object to LVTI variable but not literal null
         Object nullObject = null;
         var var = nullObject;
+
+        // Invalid var declarations:
+
+        // cannot use var declaration in a compound statement
+        //var j = 0, k = 0;
+
+        // again, cannot use var declaration in a compound statement
+        //var m, n = 0;
+
+        // Cannot declare a var variable without also initializing it
+        //var someObject;
+
+        // Cannot assign null to var variable, type cannot be inferred
+        //var newvar = null;
+
+        // Cannot use array initializer in var declaration/initialization
+        //var myArray = {"A", "B"};
+
+        // Cannot have an array of var
+        //var[]newarray = new int[2];
     }
 }
